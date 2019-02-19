d38615ee63a83d11681044f951e76a2472967dfa
sumedha <sumedhanarayanan@gmail.com>
2019-02-19 19:50:41 +0000

    Complete exercise 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,8 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+
+@store3 = Store.find_by(id: 3)
+puts "Destroying 3rd store"
+@store3.destroy
+puts "Number of stores: #{Store.count}"
