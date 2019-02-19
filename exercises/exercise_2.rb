5955d1d06379763e8285b23d779525f6769fcc43
sumedha <sumedhanarayanan@gmail.com>
2019-02-19 19:46:49 +0000

    Complete exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,11 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+@store2 = Store.second
+
+@store1.name = "Burnaby - Metrotown"
+
+puts "Store 1 is #{@store1.name} "
+puts "Store 2 is #{@store2.name}"
